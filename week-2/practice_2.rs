01462663a2fc99400fb9a501a31802d297e764b0
blankboy8 <Olowofelakevin@gmail.com>
2025-10-06 21:06:28 +0100

    (My second commit)

---
+++ week-2/practice_2.rs
@@ -0,0 +1,10 @@
+fn main() {
+	println!();  //prints just a
+    
+    println!("Hello");//prints
+    
+    println!("format{} arguments" , "some");//prints format some arguments
+    
+    println!("my name is {}. I am the class rep of stream 1", "Dagogo william-jumbo");
+
+}
