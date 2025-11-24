5ae7deb9ed38b13340c48418f9907b4e72e67b99
blankboy8 <Olowofelakevin@gmail.com>
2025-11-24 09:36:29 +0100

    my practice

---
+++ week-9/practice_4/target/debug/deps/practice_4.d
@@ -0,0 +1,5 @@
+C:\Users\olowo\Documents\K.olowofelaCOS101\week-9\practice_4\target\debug\deps\practice_4.d: src\main.rs
+
+C:\Users\olowo\Documents\K.olowofelaCOS101\week-9\practice_4\target\debug\deps\practice_4.exe: src\main.rs
+
+src\main.rs:
